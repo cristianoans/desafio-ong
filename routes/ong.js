d10154936d0ac8a36dcd34b15ac739326e39f59d
unknown <cristianoans@hotmail.com>
2023-04-19 17:42:56 -0300

    implementação do método GET na rota ong

--- routes/ong.js
+++ routes/ong.js
@@ -5,7 +5,13 @@ const Ong = require('../models/Ong')
 ong.route('/')
     .get(async (req, res) => {
         
-        res.json({ mensagem: "rota ong GET" })
+        try {
+            const ongs = await Ong.findAll();
+            res.status(200).json(ongs);
+        } catch (err) { 
+            res.status(500).json(err);
+        }
+
     })
     .post(async (req, res) => {
 
@@ -17,7 +23,7 @@ ong.route('/')
 
         try {
             const novaOng = await Ong.create(req.body);
-            res.status(201).json({mensagem: `Ong criada com sucesso, id: ${novaOng.id}` });
+            res.status(201).json({ mensagem: `Ong criada com sucesso, id: ${novaOng.id}` });
         } catch (err) {
             res.status(400).json(err);
         }
