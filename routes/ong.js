b4874f5932f5f722a2e70cd80983ece42f92496c
unknown <cristianoans@hotmail.com>
2023-04-20 10:09:27 -0300

    implementado o método PUT da rota ong

--- routes/ong.js
+++ routes/ong.js
@@ -30,7 +30,25 @@ ong.route('/')
 
     })
     .put(async (req, res) => {
-        res.json({ mensagem: "rota ong PUT" })
+        const { nome, tipo, desc, cnpj, data_criacao } = req.body;
+
+        if (!nome || !tipo || !desc || !cnpj || !data_criacao) {
+            return res.status(400).json({ mensagem: "campo obrigatório não informado" });
+        }
+
+        const ongEncontrada = await Ong.findOne({ where: { cnpj } });
+
+        if (!ongEncontrada) {
+            return res.status(400).json({ mensagem: "Ong não encontrada" });
+        }
+
+        try {
+            await ongEncontrada.update({ nome, tipo, desc, data_criacao });
+            res.status(200).json({ mensagem: `Ong atualizado com sucesso, id: ${ongEncontrada.id}` });
+        } catch (err) {
+            res.status(500).json(err);
+        }
+
     })
     .delete(async (req, res) => {
         const { id } = req.body;
@@ -39,10 +57,16 @@ ong.route('/')
             return res.status(400).json({ mensagem: "campo obrigatório não informado" });
         }
 
-        try{
-            const response = await Ong.destroy({where: {id: id}});
+        const ongEncontrada = Ong.findByPk(id);
+
+        if (!ongEncontrada) {
+            return res.status(400).json({ mensagem: "Ong não encontrada" });
+        }
+
+        try {
+            const response = await Ong.destroy(ongEncontrada);
             res.status(200).json({ mensagem: `Ong deletada com sucesso, id: ${id}` });
-        }catch(err){
+        } catch (err) {
             res.status(500).json(err);
         }
 
